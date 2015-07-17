63db283c8e84a7648ec9cf6958a4366d71eb4377
Jeff Mattfield <jeff.mattfield@quantumretail.com>
2015-07-17 14:39:56 -0500

    Set up package repos; disable menu bar and startup screen

--- init.el
+++ init.el
@@ -1,7 +1,13 @@
 (require 'package)
-(add-to-list 'package-archives 
+(add-to-list 'package-archives
     '("marmalade" .
       "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+    '("gnu" .
+      "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives
+    '("melpa" .
+      "http://melpa.org/packages/"))
 (package-initialize)
 
 ;;; Super key for OSX
@@ -12,3 +18,9 @@
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
+
+;;; Disable menu bar
+(menu-bar-mode -1)
+
+;;; Disable welcome screen
+(setq inhibit-startup-screen t)
