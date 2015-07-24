072a62b84afcca6d67c9e57564b5244ce26e4c63
Jeff Mattfield <jeff.mattfield@quantumretail.com>
2015-07-24 11:57:23 -0500

    Add several new settings and behaviors

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+;;; Package management
 (require 'package)
 (add-to-list 'package-archives
     '("marmalade" .
@@ -8,10 +9,26 @@
 (add-to-list 'package-archives
     '("melpa" .
       "http://melpa.org/packages/"))
-(package-initialize)
+(package-initialize) 
+(setq jpm-required-packages
+      (list 'win-switch 'org 'launchctl))
+(dolist (package jpm-required-packages)
+  (when (not (package-installed-p package))
+    (package-refresh-contents)
+    (package-install package)))
+
+;;; Backup and auto-save
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
 
 ;;; Super key for OSX
 (setq mac-option-modifier 'super)
+
+;;; Turn other-window into a window switching mode
+(require 'win-switch)
+(global-set-key "\C-xo" 'win-switch-dispatch)
 
 ;;; Org Mode setup
 (global-set-key "\C-cl" 'org-store-link)
@@ -19,8 +36,36 @@
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
 
-;;; Disable menu bar
-(menu-bar-mode -1)
+;;; Customize line numbering
+(defun linum-format-func (line)
+  (let ((w (length (number-to-string (count-lines (point-min) (point-max))))))
+    (propertize (format (format "%%%dd " w) line) 'face 'linum)))
+(setq linum-format 'linum-format-func)
+(add-hook 'prog-mode-hook 'linum-mode)
 
-;;; Disable welcome screen
-(setq inhibit-startup-screen t)
+;;; All of the settings I was too lazy to type by hand
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(ido-mode (quote both) nil (ido))
+ '(inhibit-startup-screen t)
+ '(menu-bar-mode nil)
+ '(org-agenda-files
+   (quote
+    ("~/org/agenda/work.org" "~/org/agenda/home.org" "~/org/agenda/education.org")))
+ '(send-mail-function (quote mailclient-send-it))
+ '(user-mail-address "jeff.mattfield@quantumretail.com")
+ '(win-switch-idle-time 5))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(message-header-cc ((t (:foreground "color-29"))))
+ '(message-header-newsgroups ((t (:foreground "color-29" :slant italic :weight bold))))
+ '(message-header-other ((t (:foreground "color-72"))))
+ '(message-header-subject ((t (:foreground "color-29" :weight bold))))
+ '(message-header-to ((t (:foreground "color-29" :weight bold)))))
