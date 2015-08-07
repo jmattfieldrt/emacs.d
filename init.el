c8969379340e22b4b670e8dc6d06fae39740ffaa
Jeff Mattfield <jeff.mattfield@quantumretail.com>
2015-08-07 15:50:54 -0500

    Move customize settings into separate file

--- init.el
+++ init.el
@@ -1,35 +1,3 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(column-number-mode t)
- '(custom-enabled-themes (quote (wombat)))
- '(global-hl-line-mode t)
- '(ido-mode (quote both) nil (ido))
- '(inhibit-startup-screen t)
- '(initial-scratch-message nil)
- '(menu-bar-mode nil)
- '(org-agenda-files
-   (quote
-    ("~/org/agenda/work.org" "~/org/agenda/home.org" "~/org/agenda/education.org")))
- '(win-switch-idle-time 3))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(hl-line ((t (:background "color-234"))))
- '(message-header-cc ((t (:foreground "color-29"))))
- '(message-header-newsgroups ((t (:foreground "color-29" :slant italic :weight bold))))
- '(message-header-other ((t (:foreground "color-72"))))
- '(message-header-subject ((t (:foreground "color-29" :weight bold))))
- '(message-header-to ((t (:foreground "color-29" :weight bold))))
- '(org-document-info ((t (:foreground "color-239"))))
- '(org-document-title ((t (:foreground "color-239" :weight bold)))))
-
 ;;; Package management
 (require 'package)
 (add-to-list 'package-archives
@@ -59,6 +27,7 @@
 (setq mac-option-modifier 'super)
 
 ;;; Turn other-window into a window switching mode
+(require 'win-switch)
 (global-set-key "\C-xo" 'win-switch-dispatch)
 
 ;;; Org Mode setup
@@ -74,3 +43,5 @@
 (setq linum-format 'linum-format-func)
 (add-hook 'prog-mode-hook 'linum-mode)
 
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file 'noerror)
